c1caf76aa581493cb19b16af24b4a240b2258d21
V.Anh Bui <v.anhbv2@vinid.net>
2020-10-28 11:20:14 +0700

    add gcs

--- gcs-int.tf
+++ gcs-int.tf
@@ -3,9 +3,9 @@
 resource "google_storage_bucket" "vinid-bucket" {
   name     = "anhbv-cloudtest"
   location      = "ASIA"
-  labels  {
+  labels = {
     av="sam"
-    
+
   }
 }
 resource "google_storage_bucket" "vinid-bucket1" {
