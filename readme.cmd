bacbb6571b94c8bf4bfc8f4030b6832a62b06a39
V.Anh Bui <v.anhbv2@vinid.net>
2020-10-28 12:20:01 +0700

    commit
    a

--- readme.cmd
+++ readme.cmd
@@ -3,4 +3,6 @@ https://app.terraform.io/app/org-vinid/workspaces/learning/runs
 Run local:
  Create API token
    - tf3 login app.terraform.io
-   
+
+tf3 state list
+plan : go to Environment Variables  add TF_CLI_ARGS_plan value -target=google_storage_bucket.vinid-bucket
